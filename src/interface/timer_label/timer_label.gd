3a04b0003cb3be7759500276674a7321dd417c5b
Konrad Nowakowski <konrad.x92@gmail.com>
2021-05-15 18:27:32 +0100

    Add timer blinking on time near ending

--- src/interface/timer_label/timer_label.gd
+++ src/interface/timer_label/timer_label.gd
@@ -4,7 +4,9 @@ export var timer_node: NodePath
 
 func _physics_process(p_delta: float) -> void:
 	if has_node(timer_node) and get_node(timer_node) is Timer:
-		_update_time_label(get_node(timer_node))
+		var timer = get_node(timer_node)
+		_update_time_label(timer)
+		_update_blinking(timer)
 
 func _update_time_label(p_timer: Timer) -> void:
 	var time_left = p_timer.time_left
@@ -12,3 +14,11 @@ func _update_time_label(p_timer: Timer) -> void:
 	var seconds = int(time_left) % 60 + (time_left - int(time_left))
 	var formatted = str(seconds).pad_zeros(2).pad_decimals(2)
 	text = "%d:%s" % [minutes, formatted]
+
+func _update_blinking(p_timer: Timer) -> void:
+	var time_left = p_timer.time_left
+	if time_left < 30 and time_left > 0:
+		var switch = int(time_left * 100) % 100 < 50
+		modulate = Color.red if switch else Color.white
+	else:
+		modulate = Color.white
