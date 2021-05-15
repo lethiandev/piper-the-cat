440480768f09dc23b03812b20980ee6e6d8b3317
Konrad Nowakowski <konrad.x92@gmail.com>
2021-05-15 14:47:17 +0100

    Add items bag controller

---
+++ src/scripts/items_bag/items_bag.gd
@@ -0,0 +1,26 @@
+extends Node
+
+const ScoreLabelScene = preload("res://effects/score_label/score_label.tscn")
+
+func _ready():
+	for child in get_children():
+		_bind_item_signals(child)
+
+func _bind_item_signals(p_item: Node) -> void:
+	if p_item.has_signal("item_moved"):
+		p_item.connect("item_moved", self, "_on_item_moved", [p_item])
+	if p_item.has_signal("item_shattered"):
+		p_item.connect("item_shattered", self, "_on_item_shattered", [p_item])
+
+func _on_item_moved(p_item: Node) -> void:
+	var score_label = ScoreLabelScene.instance()
+	score_label.text = str(p_item.move_score)
+	score_label.follow_origin = p_item.global_transform.origin
+	add_child(score_label)
+
+func _on_item_shattered(p_item: Node) -> void:
+	var score_label = ScoreLabelScene.instance()
+	score_label.text = str(p_item.shatter_score)
+	score_label.follow_origin = p_item.global_transform.origin
+	score_label.modulate = Color.yellow
+	add_child(score_label)
