f7f8b0e3f37a872cb4a6f34d378fd91a37b40f3f
Konrad Nowakowski <konrad.x92@gmail.com>
2021-05-16 20:57:03 +0100

    Fix item moved event

--- src/items/item_base/item_base.gd
+++ src/items/item_base/item_base.gd
@@ -5,9 +5,14 @@ signal item_moved()
 export var move_score: int = 50
 
 var has_moved = false
+var start_position = Vector3()
+
+func _ready() -> void:
+	start_position = global_transform.origin
 
 func _integrate_forces(p_state: PhysicsDirectBodyState) -> void:
-	if p_state.linear_velocity.length_squared() > 1.0:
+	var origin = p_state.transform.origin
+	if start_position.distance_squared_to(origin) > 0.1:
 		_handle_item_moved()
 
 func _handle_item_moved() -> void:
