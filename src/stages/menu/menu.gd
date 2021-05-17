39d073f6defaa78a3eb55af59b7459179ee867c6
Konrad Nowakowski <konrad.x92@gmail.com>
2021-05-17 04:53:26 +0100

    Add fade out/in for menu

--- src/stages/menu/menu.gd
+++ src/stages/menu/menu.gd
@@ -1,7 +1,12 @@
 extends Node
 
 func _ready():
-	$Control/VBoxContainer/Button.connect("pressed", self, "_on_start_pressed")
+	get_tree().set_pause(false)
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	$Control/VBoxContainer/Button.connect("pressed", self,
+		"_on_start_pressed", [], CONNECT_ONESHOT)
+	Transition.fade_in()
 
 func _on_start_pressed() -> void:
+	yield(Transition.fade_out(), "completed")
 	get_tree().change_scene("res://stages/playground/playground.tscn")
