26328cebeab799242857dc55f8748ae0626b7f97
Konrad Nowakowski <konrad.x92@gmail.com>
2021-05-17 03:36:52 +0100

    Add new props to rooms

--- src/stages/playground/playground.gd
+++ src/stages/playground/playground.gd
@@ -5,7 +5,7 @@ func _ready() -> void:
 	GameState.connect("game_ready", self, "_on_game_ready")
 	GameState.connect("game_ended", self, "_on_game_ended")
 	GameState.game_start(round_time)
-	get_tree().set_pause(true)
+	#get_tree().set_pause(true)
 	Transition.fade_in()
 
 func _on_game_ready() -> void:
