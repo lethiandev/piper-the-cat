07329befbe014ab29ea8a7c0d58da54be11143d0
Konrad Nowakowski <konrad.x92@gmail.com>
2021-05-17 01:54:42 +0100

    Add game end handler

--- src/stages/playground/playground.gd
+++ src/stages/playground/playground.gd
@@ -3,8 +3,12 @@ extends Node
 func _ready() -> void:
 	var round_time = 3.0 * 60.0
 	GameState.connect("game_ready", self, "_on_game_ready")
+	GameState.connect("game_ended", self, "_on_game_ended")
 	GameState.game_start(round_time)
 	get_tree().set_pause(true)
 
 func _on_game_ready() -> void:
 	get_tree().set_pause(false)
+
+func _on_game_ended() -> void:
+	get_tree().set_pause(true)
