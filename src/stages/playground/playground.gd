aee6f42b4025797e4061ed75e9f4151e2b9e43eb
Konrad Nowakowski <konrad.x92@gmail.com>
2021-05-17 01:52:21 +0100

    Add game pause on ready countdown

--- src/stages/playground/playground.gd
+++ src/stages/playground/playground.gd
@@ -2,4 +2,9 @@ extends Node
 
 func _ready() -> void:
 	var round_time = 3.0 * 60.0
+	GameState.connect("game_ready", self, "_on_game_ready")
 	GameState.game_start(round_time)
+	get_tree().set_pause(true)
+
+func _on_game_ready() -> void:
+	get_tree().set_pause(false)
